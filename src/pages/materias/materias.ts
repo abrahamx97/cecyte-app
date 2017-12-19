74f1219c34ae028d900338d497036f32fe363adb
abrahamx97 <abrahamx97@gmail.com>
2017-12-19 13:21:42 -0600

    agregado dinámico de las materias desde el servidor

--- src/pages/materias/materias.ts
+++ src/pages/materias/materias.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { NavigatorProvider } from '../../providers/navigator/navigator'
 import { HelperProvider } from '../../providers/helper/helper'
+import { JwtHelperService } from "@auth0/angular-jwt";
 import { _MasterPage } from '../';
 import { MateriaPage } from '../'
+import { Storage } from "@ionic/storage";
 
 @IonicPage()
 @Component({
@@ -12,32 +14,26 @@ import { MateriaPage } from '../'
 })
 export class MateriasPage extends _MasterPage {
 
-    materias: Array<any> = [
-        { id: 0, nombre: 'List Item Zero' },
-        { id: 1, nombre: 'List Item One' },
-        { id: 2, nombre: 'List Item Two' },
-        { id: 3, nombre: 'List Item Three' },
-        { id: 4, nombre: 'List Item Four' },
-        { id: 5, nombre: 'List Item Five' },
-        { id: 6, nombre: 'List Item Six' },
-        { id: 7, nombre: 'List Item Seven' },
-        { id: 8, nombre: 'List Item Eight' },
-        { id: 9, nombre: 'List Item Nine' },
-    ];
+    materias: Array<any> = [  ];
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private navigatorProvider: NavigatorProvider, private helperProvider: HelperProvider) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, private navigatorProvider: NavigatorProvider, private helperProvider: HelperProvider, private readonly storage: Storage, jwtHelper: JwtHelperService) {
         super();
-        this.getCalificaciones('16427070078841')
+        this.storage.get('token').then(jwt => {
+            if(jwt){
+                let usuario = jwtHelper.decodeToken(jwt).usuario
+                this.getCalificaciones(usuario) 
+            }
+        })
     }
 
     ionViewDidLoad() {
 
     }
 
-    getCalificaciones(matricula){
-        this.helperProvider.getCalificaciones(matricula).subscribe(
+    getCalificaciones(usuario){
+        this.helperProvider.getCalificaciones(usuario).subscribe(
             response => {
-                console.log(`EL RESPONSE ES ${response.body['data'][0].docente}`)
+                this.materias=response.body['data']
             },
             error => {
 
