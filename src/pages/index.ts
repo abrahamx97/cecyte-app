fa8c245a0e0e5436b4395e1f92fb7b96f55e8653
abrahamx97 <abrahamx97@gmail.com>
2018-01-01 17:32:42 -0600

    cordova-plugin-statusbar agregado

--- src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export { _DetailPage } from './_DetailPage';
-export { _MasterPage } from './_MasterPage';
-export { MateriaPage } from './materia/materia';
-export { MateriasPage } from './materias/materias';
-export { PlaceholderPage } from './placeholder/placeholder';
