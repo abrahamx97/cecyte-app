58e71ad435fe1530ab077a39affd653d26402c66
abrahamx97 <abrahamx97@gmail.com>
2018-01-01 17:36:37 -0600

    splash e icono rediseñados

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+/*export { _DetailPage } from './_DetailPage';
+export { _MasterPage } from './_MasterPage';
+export { MateriaPage } from './materia/materia';
+export { MateriasPage } from './materias/materias';
+export { PlaceholderPage } from './placeholder/placeholder';*/
